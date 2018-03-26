a27df43b84fbc057b06a52284b29abc34da4feea
Christian Powell <christiant.powell@gmail.com>
2018-03-26 11:27:32 -0500

    Merge jQuery branch to master

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    var paragraphs = $('p');
+    paragraphs.css({
+        'color': 'pink',
+        'font-family': 'Veranda',
+        'font-weight': 'bold',
+    });
+});
